4fc73253770f1905b9cab6f073e3b8134248187e
ar.delsaut <ar.delsaut@outlook.com>
2023-05-10 01:36:48 +0200

    reseaulocal edit

--- archives/Scripts/Menu Programmes et Applications/MenuAutre/1.PartagerUnDossierRéseauLocal.ps1
+++ archives/Scripts/Menu Programmes et Applications/MenuAutre/1.PartagerUnDossierRéseauLocal.ps1
@@ -2,9 +2,9 @@
 # 8.PartagerUnDossierRéseauLocal.ps1
 # V:\03.PC\01.WINDOWS\04.COMPILATION_SCRIPTS\Menu_ALL-v2\archives\Scripts\8.PartagerUnDossierRéseauLocal.ps1
 Clear-Host
-$DossierPartage = Read-Host "Path du Dossier à partager"
+$DossierPartage = Read-Host "Quel est le Path du Dossier à partager?"
 Clear-Host
-$NomNouveauPartage = Read-Host "Nom du dossier partagé"
+$NomNouveauPartage = Read-Host "Nom du partage?"
 New-SmbShare -Name "$NomNouveauPartage" -Path "$DossierPartage" -FullAccess "tout le monde"
 Write-Host "Dossier $DossierPartage partagé. Nom du Partage: $NomNouveauPartage" -ForegroundColor Green
 Pause
